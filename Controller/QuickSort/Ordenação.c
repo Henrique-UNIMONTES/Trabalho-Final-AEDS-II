7ece68bb622711852592f94f63e77cfce5af452f
joaogabrielpinheiro-SI <jgpomoc@gmail.com>
2024-12-02 04:19:25 -0300

    Update Ordenação.c

---
+++ Controller/QuickSort/Ordenação.c
@@ -0,0 +1,9 @@
+#include 'Ordenação.h'
+  
+int extractNumber(const char* str);
+
+int partition(char* arr[], int low, int high);
+
+void quickSort(char* arr[], int low, int high);
+
+void printArray(char* arr[], int size);
