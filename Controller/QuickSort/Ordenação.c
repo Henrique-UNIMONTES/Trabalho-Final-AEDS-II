45a2a6aa1e662fc1e02b2bff8873c2d2e9b9d782
joaogabrielpinheiro-SI <jgpomoc@gmail.com>
2024-12-02 04:21:25 -0300

    Update Ordenação.c

--- Controller/QuickSort/Ordenação.c
+++ Controller/QuickSort/Ordenação.c
@@ -1,9 +1,24 @@
-#include 'Ordenação.h'
-  
-int extractNumber(const char* str);
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int partition(char* arr[], int low, int high);
-
-void quickSort(char* arr[], int low, int high);
-
-void printArray(char* arr[], int size);
+void Qsort(int v[], int ini, int fim){
+  int i, j, pivo, aux;
+  i=ini;
+  j=fim;
+  pivo = ini;
+  if(ini>=fim)return;
+  while(i<j){
+    while(v[i]<v[pivo]) i++;
+    while(v[j]>v[pivo]) j--;
+    if(i<=j){
+      aux=v[i];
+      v[i]=v[j];
+      v[j]=aux;
+      if(pivo==i) pivo = j;
+      else if (pivo==j) pivo = i;
+    }
+  }
+  Qsort(v, ini, pivo-1);
+  Qsort(v, pivo+1, fim);
+}
