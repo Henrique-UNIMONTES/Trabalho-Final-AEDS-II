d8dc0a2adcd4fe6d8867fb24eee857edcb9a5adf
joaogabrielpinheiro-SI <jgpomoc@gmail.com>
2024-12-05 05:11:34 -0300

    Update Ordenação.c

---
+++ Controller/QuickSort/Ordenação.c
@@ -0,0 +1,16 @@
+#include 'Ordenação.h'
+
+//Extração de número da string do id
+int extractNumber(const char* str);
+
+//troca duas strings em um array
+void swap(char* arr[], int i, int j);
+
+//faz a partição e define o pivô para o quicksort
+int partition(char* arr[], int low, int high);
+
+//quicksort principal
+void quickSort(char* arr[], int low, int high);
+
+//impressão de array usada para teste
+void printArray(char* arr[], int size);
